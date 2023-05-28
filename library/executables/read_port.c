7316a271c5ecfc4d62f7999f41154c779330b229
guust ysebie <guust.ysebie@gmail.com>
2023-05-28 19:26:28 +0200

    ff

---
+++ library/executables/read_port.c
@@ -0,0 +1,95 @@
+//
+// Created by guust on 5/28/23.
+//
+
+#define SERIALTERMINAL      "/dev/ttyUSB0"
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+
+int set_interface_attribs(int fd, int speed)
+{
+    struct termios tty;
+
+    if (tcgetattr(fd, &tty) < 0) {
+        printf("Error from tcgetattr: %s\n", strerror(errno));
+        return -1;
+    }
+
+    cfsetospeed(&tty, (speed_t)speed);
+    cfsetispeed(&tty, (speed_t)speed);
+
+    tty.c_cflag |= CLOCAL | CREAD;
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8;         /* 8-bit characters */
+    tty.c_cflag &= ~PARENB;     /* no parity bit */
+    tty.c_cflag &= ~CSTOPB;     /* only need 1 stop bit */
+    tty.c_cflag &= ~CRTSCTS;    /* no hardware flowcontrol */
+
+    tty.c_lflag |= ICANON | ISIG;  /* canonical input */
+    tty.c_lflag &= ~(ECHO | ECHOE | ECHONL | IEXTEN);
+
+    tty.c_iflag &= ~IGNCR;  /* preserve carriage return */
+    tty.c_iflag &= ~INPCK;
+    tty.c_iflag &= ~(INLCR | ICRNL | IUCLC | IMAXBEL);
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY);   /* no SW flowcontrol */
+
+    tty.c_oflag &= ~OPOST;
+
+    tty.c_cc[VEOL] = 0;
+    tty.c_cc[VEOL2] = 0;
+    tty.c_cc[VEOF] = 0x04;
+
+    if (tcsetattr(fd, TCSANOW, &tty) != 0) {
+        printf("Error from tcsetattr: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+
+int main()
+{
+    char *portname = SERIALTERMINAL;
+    int fd;
+    int wlen;
+
+    fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
+    if (fd < 0) {
+        printf("Error opening %s: %s\n", portname, strerror(errno));
+        return -1;
+    }
+    /*baudrate 115200, 8 bits, no parity, 1 stop bit */
+    set_interface_attribs(fd, B115200);
+
+
+    char read_buf [1024];
+
+    // Normally you wouldn't do this memset() call, but since we will just receive
+    // ASCII data for this example, we'll set everything to 0 so we can
+    // call printf() easily.
+    memset(&read_buf, '\0', sizeof(read_buf));
+
+    // Read bytes. The behaviour of read() (e.g. does it block?,
+    // how long does it block for?) depends on the configuration
+    // settings above, specifically VMIN and VTIME
+    int num_bytes = read(fd, &read_buf, sizeof(read_buf));
+    for (int i = 0; i < num_bytes ; ++i) {
+        for (int j = 0; j < 20 ; ++j) {
+            i++;
+            if(i > num_bytes){
+                break;
+            }
+            printf("%x ", read_buf[i]);
+        }
+        printf("\n");
+    }
+
+
+
+}
+
